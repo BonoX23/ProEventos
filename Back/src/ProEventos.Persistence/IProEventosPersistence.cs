06c516eb2479d381bd785b7ea33af1ec0a52ef37
Ricardo Bono <ricardo.bonos@gmail.com>
2024-04-26 03:18:39 -0300

    Criando Plasses de persistencia

--- Back/src/ProEventos.Persistence/IProEventosPersistence.cs
+++ Back/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -5,7 +5,7 @@ namespace ProEventos.Persistence
 {
     public interface IProEventosPersistence
     {
-         void add<T>(T entity) where T: class;
+         void Add<T>(T entity) where T: class;
          void Update<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          void DeleteRange<T>(T[] entity) where T: class;
@@ -20,8 +20,8 @@ namespace ProEventos.Persistence
 
          //Palestrantes
 
-         Task<Evento[]> GetAllPalestrantesByNomeAsync(string nome, bool IncludePalestrantes);
-         Task<Evento[]> GetAllPalestrantesAsync(bool IncludePalestrantes);
-         Task<Evento> GetPalestranteByIdAsync(int palestranteId, bool IncludePalestrantes);
+         Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool IncludePalestrantes);
+         Task<Palestrante[]> GetAllPalestrantesAsync(bool IncludePalestrantes);
+         Task<Palestrante> GetPalestranteByIdAsync(int palestranteId, bool IncludePalestrantes);
     }
 }
