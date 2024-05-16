e9d6318e5f9f5fe26d7d6f6bda2cfdd795e1ecf8
Ricardo Bono <ricardo.bonos@gmail.com>
2024-05-16 01:59:17 -0300

    Ajustando rotas e subrotas

--- Front/ProEventos-App/src/app/Shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/Shared/titulo/titulo.component.ts
@@ -1,20 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.scss']
+  styleUrls: ['./titulo.component.scss'],
 })
-export class TituloComponent implements OnInit 
-{
+export class TituloComponent implements OnInit {
   @Input() titulo: string = '';
   @Input() iconclass = 'fa fa-user';
   @Input() subtituto = 'Desde 2021';
   @Input() botaoListar = false;
-  
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  listar(): void {
+    this.router.navigate([`/${this.titulo.toLocaleLowerCase()}/lista`]);
   }
-
 }
--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { TituloComponent } from './Shared/titulo/titulo.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 
 @NgModule({
   declarations: [	
@@ -32,7 +34,9 @@ import { TituloComponent } from './Shared/titulo/titulo.component';
     PerfilComponent,
     NavComponent,
     TituloComponent,
-    DateTimeFormatPipePipe
+    DateTimeFormatPipePipe,
+    EventoDetalheComponent,
+    EventoListaComponent
    ],
   imports: [
     BrowserModule,
