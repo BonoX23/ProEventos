7bdef6cb9206d49233b1b12c2520a8a9e18266b4
Ricardo Bono <ricardo.bonos@gmail.com>
2024-05-22 21:33:01 -0300

    Criando confirmador de senha

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from '@app/Helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  get f(): any {
+    return this.form.controls;
   }
 
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void 
+  {
+    const formOptions: AbstractControlOptions = 
+    {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha'),
+    };
+
+    this.form = this.fb.group(
+      {
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        userName: ['', Validators.required],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmeSenha: ['', Validators.required],
+      },
+      formOptions
+    );
+  }
 }
